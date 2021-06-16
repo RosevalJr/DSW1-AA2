898cec12baaa95cb8054774fb25b8295e54f2e43
RosevalJr <rdmalajr@gmail.com>
2021-06-16 04:04:05 -0300

    Opção de upload de currículo e visualização de currículo para profissional.

--- AA2/src/main/java/br/ufscar/dc/dsw/controller/ProfissionalController.java
+++ AA2/src/main/java/br/ufscar/dc/dsw/controller/ProfissionalController.java
@@ -1,15 +1,23 @@
 package br.ufscar.dc.dsw.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.ufscar.dc.dsw.dao.ICandidaturaDAO;
 import br.ufscar.dc.dsw.dao.IProfissionalDAO;
@@ -30,6 +38,9 @@ public class ProfissionalController {
 	
 	@Autowired
 	private IVagaDAO vagaDAO;
+	
+	@Autowired
+	ServletContext context;
 	
 	@GetMapping("/listarCandidaturas")
 	public String listarCandidaturas(ModelMap model, Principal principal) {
@@ -60,14 +71,47 @@ public class ProfissionalController {
 	}
 	
 	@GetMapping("/aplicarVaga/{id}")
-	public String aplicarVaga(@PathVariable("id") long id, ModelMap model, Principal principal) {
+	public String aplicarCurriculo(@PathVariable("id") long id, ModelMap model, Principal principal) throws java.text.ParseException {
+		
+		Vaga vaga = vagaDAO.findById(id);
+		
+		Profissional profissional = profissionalDAO.findByUsername(principal.getName());
+		//List<Vaga> todasVagas = vagaDAO.findAll();
+		//List<Vaga> vagasAbertas =  new ArrayList<Vaga>();
+		
+		if(vaga.isAberta()) {
+			model.addAttribute("vaga", vaga);
+			return "profissional/listaVaga";
+		}
+		else
+			return "redirect:/profissionais/aplicar";
+	}
+	
+	
+	// Aqui chega o arquivo em file por um post
+	@PostMapping("/uploadFile/{id}")
+	public String aplicarVaga(@RequestParam("file") MultipartFile file, @PathVariable("id") long id, ModelMap model, Principal principal, RedirectAttributes attr) throws IllegalStateException, IOException {
+		
+		String fileName = file.getOriginalFilename();
+		
+		String uploadPath = context.getRealPath("") + File.separator + "upload";
+		File uploadDir = new File(uploadPath);
+		if (!uploadDir.exists()) {
+			uploadDir.mkdir();
+		}
+		
+		file.transferTo(new File(uploadDir, fileName));
+		
+		attr.addFlashAttribute("sucess", "File " + fileName + " has uploaded successfully!");
+		
+		
 		
 		Profissional profissional = profissionalDAO.findByUsername(principal.getName());
 		Vaga vaga = vagaDAO.findById(id);
 		
-		
+
 		Candidatura candidatura = new Candidatura();
-		candidatura.setCurriculo("curriculo.pdf");
+		candidatura.setCurriculo(fileName);
 		candidatura.setProfissional(profissional);
 		candidatura.setVaga(vaga);
 		candidatura.setStatus("ABERTO");
